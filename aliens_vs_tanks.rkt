219de330994cee7c9c83dc15143f59b2f51ad109
bofmar <bofmar@outlook.com>
2023-08-03 21:55:22 +0300

    Create linux executable

--- aliens_vs_tanks.rkt
+++ aliens_vs_tanks.rkt
@@ -1,3 +1,4 @@
+#! /usr/bin/env racket
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname aliens_vs_tanks) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
